32c067641b2dda13e8a2c1a251d6a5c53fc3ab7e
gu2nick3-creator <gu2nick3@gmail.com>
2026-02-24 04:11:36 -0300

    Create ProductPage.tsx

---
+++ src/pages/ProductPage.tsx
@@ -0,0 +1,8 @@
+export default function ProductPage() {
+  return (
+    <main style={{ padding: 24 }}>
+      <h1>ProductPage</h1>
+      <p>Arquivo criado só pra corrigir o build.</p>
+    </main>
+  );
+}
