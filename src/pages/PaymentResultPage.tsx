8b63ee97ecddf900c498aecb76e34bb8af30a34b
gu2nick3-creator <gu2nick3@gmail.com>
2026-02-24 04:23:42 -0300

    Add PaymentResultPage component

---
+++ src/pages/PaymentResultPage.tsx
@@ -0,0 +1,8 @@
+export default function PaymentResultPage() {
+  return (
+    <div style={{ padding: 24 }}>
+      <h1>Resultado do pagamento</h1>
+      <p>Verificando seu pagamento...</p>
+    </div>
+  );
+}
