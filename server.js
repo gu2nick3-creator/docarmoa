66a7b4e2543881838122ef15b88455a188206bea
gu2nick3-creator <gu2nick3@gmail.com>
2026-02-23 22:54:54 -0300

    Fix indentation and add newline at end of file

--- server.js
+++ server.js
@@ -4,5 +4,8 @@ import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+// entra na pasta /server (pra distPath ../dist funcionar)
 process.chdir(path.join(__dirname, "server"));
+
+// inicia o backend real
 await import("./server/src/index.js");
